0ecc0d3f89b85a2902650116eba525e3a967b022
Doe <doe.nannong@sunline-i.com>
2022-05-16 15:03:25 +0700

    add semantic ui

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ export class NavbarComponent implements OnInit {
     profile = {
         profile : [
             {
-                name: 'defalut profile'
+                name: 'default profile'
             }
         ]
     }
